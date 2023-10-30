dd66f6d76a9c596d282f3630b908bfe0d9cc55e4
Abderrahman CHEIKH AHMEDOU <a.sidimoctar@gti-intl.com>
2023-10-30 10:48:16 +0000

    TD1/ exo4: correction

---
+++ 1.src/TD1/Exo4.js
@@ -0,0 +1,26 @@
+// FALSY VALUES IN JS are 
+//  false, null, undefined, 0 and ''
+let bool = false;
+if(!bool) {
+    console.log("bool is falsy");
+}
+
+bool = null;
+if(!bool) {
+    console.log("bool is falsy");
+}
+
+bool = undefined;
+if(!bool) {
+    console.log("bool is falsy");
+}
+
+bool = 0;
+if(!bool) {
+    console.log("bool is falsy");
+}
+
+bool = '';
+if(!bool) {
+    console.log("bool is falsy");
+}
