56f0be0ddc9d42640cc7f12dc6f62d842d62c883
Abderrahman CHEIKH AHMEDOU <a.sidimoctar@gti-intl.com>
2023-10-30 10:36:17 +0000

    TD1/ Exo3 : correction

---
+++ 1.src/TD1/Exo3.js
@@ -0,0 +1,13 @@
+let addNumbers = (...numbers) => {
+    let sum = 0;
+    for(number of numbers) {
+        sum += number;
+    }
+
+    console.log(sum);
+};
+
+addNumbers();
+addNumbers(5);
+addNumbers(5, 6);
+addNumbers(5, 6, 7);
