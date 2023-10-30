0635811cdc7ca11186df6dbea4435b864dd1634c
Abderrahman CHEIKH AHMEDOU <a.sidimoctar@gti-intl.com>
2023-10-30 10:13:04 +0000

    TD1/ exo 1 et 2 : correction

---
+++ 1.src/TD1/Exo1.js
@@ -0,0 +1,17 @@
+if(true) {
+    let i = 5;
+    i++;
+}
+console.log(i);
+
+if(true) {
+    const j = 5;
+    j++;
+}
+console.log(j);
+
+if(true) {
+    var k = 5;
+    k++;
+}
+console.log(k);
---
+++ 1.src/TD1/Exo2.js
@@ -0,0 +1,9 @@
+function addNumbers(a, b) {
+  return a + b;
+}
+
+console.log(addNumbers(9, 3)); 
+
+
+const addNumbersArrow = (a, b) => a + b;
+console.log(addNumbersArrow(2, 3));
